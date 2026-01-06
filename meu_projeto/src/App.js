7aa31ecaf392dcfa50ef641a1c74d4a9b02bf3ac
André Diogo <andrediogods@gmail.com]>
2026-01-05 23:58:04 -0300

    Alterações e inserção de novos elementos.

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const name = "Silvan"
+  const newName = name.toUpperCase()
+  function sum (a, b) {
+    return a + b
+  }
+
+  const url = "https://i.redd.it/haaland-after-letting-his-hair-down-v0-ahlbacmqnawa1.jpg?width=614&format=pjpg&auto=webp&s=c6edbf10fd33b389c8570c1c9c5568fc2fa5fb5c"
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>O {newName} é tchola!</h2>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem"></img>
     </div>
   );
 }
