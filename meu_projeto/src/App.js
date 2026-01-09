61b33f299d570ad9507f6a930d530e83b877e2ac
André Diogo <andrediogods@gmail.com]>
2026-01-09 01:45:41 -0300

    Aplicação de props

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 function App() {
 
-  const name = "Silvan"
-  const newName = name.toUpperCase()
-  function sum (a, b) {
-    return a + b
-  }
+  const nome = "Maria"
 
   const url = "https://i.redd.it/haaland-after-letting-his-hair-down-v0-ahlbacmqnawa1.jpg?width=614&format=pjpg&auto=webp&s=c6edbf10fd33b389c8570c1c9c5568fc2fa5fb5c"
 
   return (
     <div className="App">
-      <h2>O {newName} é tchola!</h2>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha Imagem"></img>
-      <HelloWorld />
+      <SayMyName nome="André" />
+      <SayMyName nome="Davi" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Rodrigo" idade="25" profissao="Assistente em TI" foto="https://s2-oglobo.glbimg.com/ne-pa7bKMxi0uOzmacyTbY3Bkkg=/0x0:984x554/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2026/u/x/qRHRwATwi4qMYzUnADfg/primeira-foto-de-maduro-preso-pelos-eua.avif"/>
     </div>
   );
 }
