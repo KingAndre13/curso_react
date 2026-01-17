405ade368c8639363e1b453c9bd29fc4ce82918e
André Diogo <andrediogods@gmail.com]>
2026-01-17 01:41:28 -0300

    Adicionado a biblioteca PropTypes

--- meu_projeto/src/components/List.jsx
+++ meu_projeto/src/components/List.jsx
@@ -4,12 +4,14 @@ function List () {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca='Ferrari'/>
-                <Item marca='Lanborghini'/>
-                <Item marca='Renault'/>
+                <Item marca={'Ferrari'}  ano_lancamento={1985} />
+                <Item marca={'Lanborghini'} ano_lancamento={1964} />
+                <Item marca={'Renault'} ano_lancamento={2006} />
+                <Item marca={'Chevrolet'} ano_lancamento={1999}/>
+                <Item />
             </ul>
         </>
     )
 }
 
-export default List
+export default List 
