665105b999bb2bbd20d8d0deccd594de2cd7ea82
André Diogo <120501460+KingAndre13@users.noreply.github.com>
2026-02-05 00:27:30 -0300

    Adcionado eventos através de props

--- meu_projeto/src/components/Evento.jsx
+++ meu_projeto/src/components/Evento.jsx
@@ -1,12 +1,20 @@
-function Evento ({ numero }) {
+import Button from './evento/Button';
+
+function Evento () {
 
     function meuEvento () {
-        alert(`O número ${numero} foi ativado!`)
+        alert(`Ativando primeiro evento!`)
     }
+
+    function segundoEvento() {
+        alert('Ativando o segundo evento!')
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
