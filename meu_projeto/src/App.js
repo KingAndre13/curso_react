2818fd1e711b117e44ae0e0ab9f80f6244ad1eca
André Diogo <120501460+KingAndre13@users.noreply.github.com>
2026-02-10 23:51:47 -0300

    Renderização Condicional

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-import Item from './components/Item';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
+
 function App() {
 
   const nome = "Maria"
@@ -15,9 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento />
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Condicional />
     </div>
   );
 }
