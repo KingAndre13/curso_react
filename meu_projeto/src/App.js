5f9eb78a32b97eb2b3f5ac5916563d1bf6351cbc
André Diogo <120501460+KingAndre13@users.noreply.github.com>
2026-02-16 14:38:44 -0300

    Renderização de Listas

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
 
-  const nome = "Maria"
-
-  const url = "https://i.redd.it/haaland-after-letting-his-hair-down-v0-ahlbacmqnawa1.jpg?width=614&format=pjpg&auto=webp&s=c6edbf10fd33b389c8570c1c9c5568fc2fa5fb5c"
+  const meusItens = ['React', 'Vue', 'Angular']
 
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   );
 }
